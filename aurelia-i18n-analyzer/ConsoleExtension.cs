52b6105c1b9a6b6269003fc4fc2dfe77e358254d
Francisco Osório <francisco.osorio@findmore.eu>
2017-12-11 16:37:19 +0000

    improve method names and added paths in args

---
+++ aurelia-i18n-analyzer/ConsoleExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace aureliai18nanalyzer
+{
+    public static class ConsoleExtension
+    {
+        public static void WriteErrorLine(string text)
+        {
+            ConsoleColor foregroundColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(text);
+            Console.ForegroundColor = foregroundColor;
+        }
+
+        public static void WriteSuccessLine(string text)
+        {
+            ConsoleColor foregroundColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(text);
+            Console.ForegroundColor = foregroundColor;
+        }
+    }
+}
--- aurelia-i18n-analyzer/Program.cs
+++ aurelia-i18n-analyzer/Program.cs
@@ -2,31 +2,53 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
-using aureliai18nanalyzer;
+using System.Linq;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 
-namespace aurelia_i18n_analyzer
+namespace aureliai18nanalyzer
 {
     class Program
     {
         static void Main(string[] args)
         {
             Stopwatch sw = new Stopwatch();
-
             sw.Start();
 
             string allFilesContent = "";
+            string directoryToBeSearched = args[0];
+            string translationFilesPath = args[1];
 
-            List<string> resourcesExtensions = new List<string>();
+            Console.WriteLine("Directory to be searched (absolute path): "+ directoryToBeSearched);
+            if (AuxMethods.DirectoryPathValidator(directoryToBeSearched))
+            {   
+                Console.WriteLine("Translation files location (absolute path): " + translationFilesPath);
+
+                if (!AuxMethods.DirectoryPathValidator(translationFilesPath))
+                {
+                    Environment.Exit(267); //ERROR_DIRECTORY The directory name is invalid.
+                }
+            }
+            else
+            {
+                Environment.Exit(267); //ERROR_DIRECTORY The directory name is invalid.
+            }
+
+            Console.WriteLine("File types to be searched: .ts .html");
+            Console.WriteLine("Language files types: .json");
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            List <string> resourcesExtensions = new List<string>();
             resourcesExtensions.Add("json");
 
             List<string> sourceExtensions = new List<string>();
             sourceExtensions.Add("ts");
             sourceExtensions.Add("html");
-                
-            List<string> resourceFiles = AuxMethods.ProcessPathFiles("/assets/locales", resourcesExtensions);
-            List<string> sourceFiles = AuxMethods.ProcessPathFiles("/src", resourcesExtensions);
+
+            List<string> resourceFiles = AuxMethods.ProcessPathFiles(translationFilesPath, resourcesExtensions);
+            List<string> sourceFiles = AuxMethods.ProcessPathFiles(directoryToBeSearched, resourcesExtensions);
 
             foreach (var item in sourceFiles)
             {
@@ -57,18 +79,18 @@ namespace aurelia_i18n_analyzer
 
                     foreach (var item in locale)
                     {
-                        locales.AddRange(AuxMethods.asd(item.Value));
+                        locales.AddRange(AuxMethods.GetJsonPaths(item.Value));
                     }
                 }
 
                 resources.Add(new Resource { Path = file, Locales = locales });
             }
 
-
+            var total = 0;
             foreach (var resource in resources)
             {
-                var total = 0;
-                Console.WriteLine($"Processing {resource.Path}");
+                var totalPerLangFile = 0;
+                ConsoleExtension.WriteSuccessLine($"Processing {resource.Path}");
 
                 foreach (var item in resource.Locales)
                 {
@@ -86,15 +108,20 @@ namespace aurelia_i18n_analyzer
                     }
                     else
                     {
-                        total++;
+                        totalPerLangFile++;
                         Console.WriteLine(item.LocalePath);
                     }
                 }
-                Console.WriteLine($"Total unused: {total}");
+                ConsoleExtension.WriteSuccessLine($"Finished. Total unused on this language file: {totalPerLangFile}");
+                total += totalPerLangFile;
+                Console.WriteLine();
             }
 
             sw.Stop();
-            Console.WriteLine("Elapsed={0}", sw.Elapsed);
+            Console.WriteLine();
+            ConsoleExtension.WriteSuccessLine($"Total unused: {total}");
+            Console.WriteLine("Elapsed = {0}", sw.Elapsed);
+            Console.ReadLine();
         }
     }
 }
